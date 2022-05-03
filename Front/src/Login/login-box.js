8b9dbc9f6d0b0695080f3b55928a29d638e5ed28
Marcos Barros <81267060+marcosbarross@users.noreply.github.com>
2022-05-03 07:57:56 -0300

    Adiciona a primeira parte do frontend

---
+++ Front/src/Login/login-box.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import login from "./login.css";
+import Books from "../Home/Books";
+
+function LoginBox() {
+    <div className="box">
+        <input type="text"></input>
+    </div>
+}
+
+export default LoginBox;
---
+++ Front/src/Login/login-page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import login from "./login.css";
+import LoginBox from "./login-box";
+
+root.render(
+    <React.StrictMode>
+        <div className="main-div">
+           <loginBox />
+        </div>
+    </React.StrictMode>
+);
