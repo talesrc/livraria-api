4bf69259c14a075c0b32d8c8c64db7a12129d3c2
Marcos Barros <81267060+marcosbarross@users.noreply.github.com>
2022-05-17 20:50:32 -0300

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,11 +8,11 @@ import { ProductCategory } from "../models/productCategory.model";
 import { Category } from "../models/category.model";
 
 export const db = new Sequelize({
-    dialect: 'mysql',
-    host: 'localhost',
+    dialect: 'mariadb',
+    host: 'db',
     port: 3306,
     username: 'root',
-    password: '',
+    password: 'senha1234',
     database: 'LIVRARIA',
     models: [User, Adress, Product, Order, OrderProduct, Category, ProductCategory],
     define: {
