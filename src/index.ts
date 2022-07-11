4982c5e25474fc3fe5c189141c0735c7fd5311a5
talesrc <taalesr@gmail.com>
2022-07-11 19:23:14 -0300

    Test is working in ts

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,7 @@
-import express from 'express';
-
-
-import { db } from './config/config';
-
-const userRouter = require('./routes/user.router');
-const productRouter = require('./routes/product.router');
-const orderRouter = require('./routes/order.router');
-const categoryRouter = require('./routes/category.router');
-var cookieParser = require('cookie-parser')
-
-
-const path = require('path');
+const app = require('./server');
 
 const PORT = 3000
 
-db.sync()
-const app = express()
-
-app.use(cookieParser())
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', categoryRouter)
-app.use('/', userRouter)
-app.use('/', productRouter)
-app.use('/', orderRouter)
-
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`)
-});
+    console.log(`Server is running at http://localhost:${PORT}`)
+  });
