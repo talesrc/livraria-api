07179151e82c302200a49be1351ad94229e780f4
talesrc <taalesr@gmail.com>
2022-07-12 22:12:26 -0300

    Adding verification of login in order create

--- src/routes/order.router.ts
+++ src/routes/order.router.ts
@@ -3,12 +3,14 @@ var router = express.Router();
 
 const orderController = require('../controllers/order.controller')
 
+const auth = require('../middlewares/auth.middleware');
+
 //Métodos para renderizar as páginas
-router.get('/admin/order', orderController.getAllOrdersPage)
+router.get('/admin/order', auth.validateToken, orderController.getAllOrdersPage)
 router.get('/cart', orderController.userCartPage)
 
 //Métodos que fazem alterações
-router.post('/user/createOrder', orderController.create)
+router.post('/user/createOrder', auth.validateToken, orderController.create)
 router.post('/order/addProduct/:id', orderController.addProductOrder)
 router.post('/order/removeProduct/:id', orderController.removeCartProduct)
 
